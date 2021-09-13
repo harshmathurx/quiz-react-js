df078404841d0426f9555ee9842f456c3fbee6e2
harshmathurx <harshmathurx@gmail.com>
2021-09-13 19:19:22 +0530

    initial commit

---
+++ src/Flashcard.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+function Flashcard({flashcard}) {
+
+    const [flip,setFlip] = useState(false);
+    const [height,setHeight] = useState('initial')
+    const frontEl = useRef();
+    const backEl = useRef();
+    const setMaxHeight = () => {
+        const frontHeight= frontEl.current.getBoundingClientRect().height;
+        const backHeight= backEl.current.getBoundingClientRect().height;
+        setHeight(Math.max(frontHeight,backHeight,100));
+        
+    }
+
+    useEffect(setMaxHeight,[flashcard.question,flashcard.answer,flashcard.options])
+    useEffect(() => {
+        window.addEventListener('resize',setMaxHeight);
+        //cleanup
+        return() => {
+            window.removeEventListener('resize',setMaxHeight);
+        }
+    },[])
+
+    return (
+        <div className={`card ${flip?'flip':''}`} onClick={() => setFlip(!flip)} style={{ height: height}}>
+            <div className="front" ref={frontEl}>
+                {flashcard.question}
+
+                <div className="flashcard-options">
+                    {flashcard.options.map(option => {
+                        return <div className="flashcard-option" key={option}>{option}</div>
+                    })}
+                </div> 
+            </div>
+
+            <div className="back" ref={backEl}>
+                {flashcard.answer}
+            </div>
+        </div>
+    )
+}
+
+export default Flashcard
